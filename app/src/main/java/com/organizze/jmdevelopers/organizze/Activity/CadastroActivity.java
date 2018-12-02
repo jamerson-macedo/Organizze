dcd6dd7f6106fd5d2c0369b6040f239f0b5c5b33
jamerson_macedo@hotmail.com <jamerson_macedo@hotmail.com>
2018-12-02 01:21:04 -0200

    Modificacoes nas telas de login e cadastro

--- app/src/main/java/com/organizze/jmdevelopers/organizze/Activity/CadastroActivity.java
+++ app/src/main/java/com/organizze/jmdevelopers/organizze/Activity/CadastroActivity.java
@@ -11,5 +11,9 @@ public class CadastroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+
+
+
     }
 }
